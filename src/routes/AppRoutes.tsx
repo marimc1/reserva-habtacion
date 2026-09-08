e03a14f85636cb960d947781821d14f08aebbc1b
marimc1 <mariana.merchz@gmail.com>
2026-09-08 17:22:45 -0400

    feat: agregar ruta de perfil de usuario

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/auth/LoginPage";
+import ProfilePage from "../pages/ProfilePage";
 import RoomsPage from "../pages/RoomsPage";
 import ReservationPage from "../pages/ReservationPage";
 import ServicesPage from "../pages/ServicesPage";
@@ -15,6 +16,7 @@ function AppRoutes() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/perfil" element={<ProfilePage />} />
         <Route path="/habitaciones" element={<RoomsPage />} />
         <Route path="/servicios" element={<ServicesPage />} />
         <Route path="/instalaciones" element={<FacilitiesPage />} />
