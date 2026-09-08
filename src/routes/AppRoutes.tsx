41c0dd2c8d37add98878a35469a0fe7317c3ce24
marimc1 <mariana.merchz@gmail.com>
2026-09-08 06:39:21 -0400

    feat: add routes for services gallery and contact

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,6 +4,9 @@ import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/auth/LoginPage";
 import RoomsPage from "../pages/RoomsPage";
 import ReservationPage from "../pages/ReservationPage";
+import ServicesPage from "../pages/ServicesPage";
+import GalleryPage from "../pages/GalleryPage";
+import ContactPage from "../pages/ContactPage";
 
 function AppRoutes() {
   return (
@@ -12,6 +15,9 @@ function AppRoutes() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/habitaciones" element={<RoomsPage />} />
+        <Route path="/servicios" element={<ServicesPage />} />
+        <Route path="/galeria" element={<GalleryPage />} />
+        <Route path="/contacto" element={<ContactPage />} />
         <Route path="/reservar" element={<ReservationPage />} />
       </Routes>
     </BrowserRouter>
