0724d46e124d80b66c7b3ac4c999a10a0d3aacff
marimc1 <mariana.merchz@gmail.com>
2026-09-08 22:51:40 -0400

    feat: add registration and reviews routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
 import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/auth/LoginPage";
+import RegisterPage from "../pages/auth/RegisterPage";
 import ProfilePage from "../pages/ProfilePage";
 import RoomsPage from "../pages/RoomsPage";
 import ReservationPage from "../pages/ReservationPage";
@@ -9,23 +9,7 @@ import ServicesPage from "../pages/ServicesPage";
 import FacilitiesPage from "../pages/FacilitiesPage";
 import GalleryPage from "../pages/GalleryPage";
 import ContactPage from "../pages/ContactPage";
-
-function AppRoutes() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/perfil" element={<ProfilePage />} />
-        <Route path="/habitaciones" element={<RoomsPage />} />
-        <Route path="/servicios" element={<ServicesPage />} />
-        <Route path="/instalaciones" element={<FacilitiesPage />} />
-        <Route path="/galeria" element={<GalleryPage />} />
-        <Route path="/contacto" element={<ContactPage />} />
-        <Route path="/reservar" element={<ReservationPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
+import ReviewsPage from "../pages/ReviewsPage";
+import AdminDashboardPage from "../pages/admin/AdminDashboardPage";
+function AppRoutes(){return <BrowserRouter><Routes><Route path="/" element={<HomePage/>}/><Route path="/login" element={<LoginPage/>}/><Route path="/registro" element={<RegisterPage/>}/><Route path="/perfil" element={<ProfilePage/>}/><Route path="/habitaciones" element={<RoomsPage/>}/><Route path="/reservar" element={<ReservationPage/>}/><Route path="/servicios" element={<ServicesPage/>}/><Route path="/instalaciones" element={<FacilitiesPage/>}/><Route path="/galeria" element={<GalleryPage/>}/><Route path="/contacto" element={<ContactPage/>}/><Route path="/opiniones" element={<ReviewsPage/>}/><Route path="/admin" element={<AdminDashboardPage/>}/></Routes></BrowserRouter>}
 export default AppRoutes;
