c2e1ad4196f3bbc2bd765452c3a5d1167193175e
marimc1 <mariana.merchz@gmail.com>
2026-09-08 22:55:11 -0400

    feat: add printable receipt route

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,16 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "../pages/HomePage";
-import LoginPage from "../pages/auth/LoginPage";
-import RegisterPage from "../pages/auth/RegisterPage";
-import ProfilePage from "../pages/ProfilePage";
-import RoomsPage from "../pages/RoomsPage";
-import ReservationPage from "../pages/ReservationPage";
-import ServicesPage from "../pages/ServicesPage";
-import FacilitiesPage from "../pages/FacilitiesPage";
-import GalleryPage from "../pages/GalleryPage";
-import ContactPage from "../pages/ContactPage";
-import ReviewsPage from "../pages/ReviewsPage";
-import PromotionsPage from "../pages/PromotionsPage";
-import AdminDashboardPage from "../pages/admin/AdminDashboardPage";
-function AppRoutes(){return <BrowserRouter><Routes><Route path="/" element={<HomePage/>}/><Route path="/login" element={<LoginPage/>}/><Route path="/registro" element={<RegisterPage/>}/><Route path="/perfil" element={<ProfilePage/>}/><Route path="/habitaciones" element={<RoomsPage/>}/><Route path="/reservar" element={<ReservationPage/>}/><Route path="/servicios" element={<ServicesPage/>}/><Route path="/instalaciones" element={<FacilitiesPage/>}/><Route path="/galeria" element={<GalleryPage/>}/><Route path="/contacto" element={<ContactPage/>}/><Route path="/opiniones" element={<ReviewsPage/>}/><Route path="/promociones" element={<PromotionsPage/>}/><Route path="/admin" element={<AdminDashboardPage/>}/></Routes></BrowserRouter>}
+import HomePage from "../pages/HomePage";import LoginPage from "../pages/auth/LoginPage";import RegisterPage from "../pages/auth/RegisterPage";import ProfilePage from "../pages/ProfilePage";import RoomsPage from "../pages/RoomsPage";import ReservationPage from "../pages/ReservationPage";import ReceiptPage from "../pages/ReceiptPage";import ServicesPage from "../pages/ServicesPage";import FacilitiesPage from "../pages/FacilitiesPage";import GalleryPage from "../pages/GalleryPage";import ContactPage from "../pages/ContactPage";import ReviewsPage from "../pages/ReviewsPage";import PromotionsPage from "../pages/PromotionsPage";import AdminDashboardPage from "../pages/admin/AdminDashboardPage";
+function AppRoutes(){return <BrowserRouter><Routes><Route path="/" element={<HomePage/>}/><Route path="/login" element={<LoginPage/>}/><Route path="/registro" element={<RegisterPage/>}/><Route path="/perfil" element={<ProfilePage/>}/><Route path="/habitaciones" element={<RoomsPage/>}/><Route path="/reservar" element={<ReservationPage/>}/><Route path="/comprobante/:id" element={<ReceiptPage/>}/><Route path="/servicios" element={<ServicesPage/>}/><Route path="/instalaciones" element={<FacilitiesPage/>}/><Route path="/galeria" element={<GalleryPage/>}/><Route path="/contacto" element={<ContactPage/>}/><Route path="/opiniones" element={<ReviewsPage/>}/><Route path="/promociones" element={<PromotionsPage/>}/><Route path="/admin" element={<AdminDashboardPage/>}/></Routes></BrowserRouter>}
 export default AppRoutes;
